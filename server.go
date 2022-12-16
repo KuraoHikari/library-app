7c05ffcb8657e19eecd7cfff5c0827130fcd4870
KuraoHikari <dewaindra705@gmail.com>
2022-12-16 22:44:32 +0800

    update ayth controller and server first hit

--- server.go
+++ server.go
@@ -1,18 +1,25 @@
 package main
 
 import (
-	"net/http"
+	"github.com/KuraoHikari/library-app/config"
+	"github.com/KuraoHikari/library-app/controller"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
-  
+
+var (
+	db *gorm.DB = config.SetupDatabaseConnection()
+	authController controller.AuthController = controller.NewAuthController()
+)
 
 func main() {
+	defer config.CloseDatabaseConnection(db)
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+	authRoutes := r.Group("api/auth")
+	{
+		authRoutes.POST("/login", authController.Login)
+		authRoutes.POST("/register", authController.Register)
+	}
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
