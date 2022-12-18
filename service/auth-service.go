d8de5ed768b38b7d027c3b4398e7f7361ba865fe
KuraoHikari <dewaindra705@gmail.com>
2022-12-18 21:15:46 +0800

    updat service

---
+++ service/auth-service.go
@@ -0,0 +1,60 @@
+package service
+
+import (
+	"log"
+
+	"github.com/KuraoHikari/library-app/dto"
+	"github.com/KuraoHikari/library-app/entity"
+	"github.com/KuraoHikari/library-app/repository"
+	"github.com/mashingan/smapping"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type AuthService interface {
+	VerifyCredential(email string, password string) interface{}
+	CreateUser(user dto.RegisterDTO) entity.User
+	FindByEmail(email string) entity.User
+	IsDuplicateEmail(email string) bool
+}
+
+type authService struct {
+	userRepository repository.UserRepository
+}
+
+func NewAuthService(userRep repository.UserRepository) AuthService {
+	return &authService{
+		userRepository: userRep,
+	}
+}
+
+func (service *authService) VerifyCredential(email string, password string) interface{}{
+	res := service.userRepository.VerifyCredential(email, password)
+	if v, ok := res.(entity.User); ok {
+		err := bcrypt.CompareHashAndPassword([]byte(v.Password), []byte(password) )
+		if err != nil {
+			log.Println(err)
+			return false
+		}else if v.Email != email {
+			return false
+		}
+		return res
+	}else {
+		return false
+	}
+}
+func (service *authService) CreateUser(user dto.RegisterDTO) entity.User{
+	userToCreate := entity.User{}
+	err := smapping.FillStruct(&userToCreate, smapping.MapFields(&user))
+	if err != nil {
+		log.Fatalf("Failed map %v", err)
+	}
+	res := service.userRepository.InsertUser(userToCreate)
+	return res
+}
+func (service *authService) FindByEmail(email string) entity.User{
+	return service.userRepository.FindByEmail(email)
+}
+func (service *authService) IsDuplicateEmail(email string) bool{
+	res := service.userRepository.IsDuplicateEmail(email)
+	return !(res.Error == nil)
+}
