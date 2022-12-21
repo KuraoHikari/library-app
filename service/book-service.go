5478d6bf276e6a2c40b9d596b66b0691f6f94bd8
KuraoHikari <dewaindra705@gmail.com>
2022-12-21 21:39:56 +0800

    update book controller and completed, fix one to many user, update user repo, completed book service

--- service/book-service.go
+++ service/book-service.go
@@ -1,14 +1,18 @@
 package service
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/KuraoHikari/library-app/dto"
 	"github.com/KuraoHikari/library-app/entity"
 	"github.com/KuraoHikari/library-app/repository"
+	"github.com/mashingan/smapping"
 )
 
 type BookService interface {
 	Insert(b dto.BookCreateDTO) entity.Book
-	Update(b dto.BookCreateDTO) entity.Book
+	Update(b dto.BookUpdateDTO) entity.Book
 	Delete(b entity.Book)
 	All() []entity.Book
 	FindByID(bookID uint64) entity.Book
@@ -23,4 +27,37 @@ func NewBookService(bookRepo repository.BookRepository) BookService {
 	return &bookService{
 		bookRepository: bookRepo,
 	}
+}
+
+func(service *bookService) Insert(b dto.BookCreateDTO) entity.Book{
+	book := entity.Book{}
+	err := smapping.FillStruct(&book, smapping.MapFields(&b))
+	if err != nil {
+		log.Fatalf("Failed map %v: ", err)
+	}
+	res := service.bookRepository.InsertBook(book)
+	return res
+}
+func(service *bookService) Update(b dto.BookUpdateDTO) entity.Book{
+	book := entity.Book{}
+	err := smapping.FillStruct(&book, smapping.MapFields(&b))
+	if err != nil {
+		log.Fatalf("Failed map %v: ", err)
+	}
+	res := service.bookRepository.UpdateBook(book)
+	return res
+}
+func(service *bookService) Delete(b entity.Book){
+	service.bookRepository.DeleteBook(b)
+}
+func(service *bookService) All() []entity.Book {
+	return service.bookRepository.AllBook()
+}
+func(service *bookService) FindByID(bookID uint64) entity.Book{
+	return service.bookRepository.FindBookByID(bookID)
+}
+func(service *bookService) IsAllowedToEdit(userID string, bookID uint64)bool{
+	b := service.bookRepository.FindBookByID(bookID)
+	id := fmt.Sprintf("%v", b.UserID)
+	return userID == id
 }
